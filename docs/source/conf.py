a94b504e3da52d9c19c61d70aaf43bc1f107cef0
Jesus Leal <jesusalbertolv@gmail.com>
2024-11-02 13:56:02 -0300

    otros cambios

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,4 +47,4 @@ language = "es"
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
+html_static_path = ["../html/_static"]
